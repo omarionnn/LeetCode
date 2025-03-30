0b6c9cee945ab450d5c206d66a7a2490e5822ce7
omarionnn <75476241+omarionnn@users.noreply.github.com>
2025-03-30 14:26:23 -0400

    Time: 7 ms (28.55%) Space: 19.2 MB (50.04%)

--- 0210-course-schedule-ii/course-schedule-ii.py
+++ 0210-course-schedule-ii/course-schedule-ii.py
@@ -2,23 +2,24 @@ class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
         in_degree = [0] * numCourses
-        
-        for course, preq in prerequisites:
-            graph[preq].append(course)
-            in_degree[course] += 1
-            
-        queue = deque([i for i in range(numCourses) if in_degree[i] == 0])
+
+        for crs, pre in prerequisites:
+            graph[pre].append(crs)
+            in_degree[crs] += 1
+
+        q = (deque(i for i in range(numCourses) if in_degree[i] == 0))
 
         order = []
 
-        while queue:
-            course = queue.popleft()
+        while q:
+            course = q.popleft()
             order.append(course)
 
             for c in graph[course]:
                 in_degree[c] -= 1
-                if in_degree[c] == 0:
-                    queue.append(c)
 
-        return order if numCourses == len(order) else []
+                if in_degree[c] == 0:
+                    q.append(c)
+
+        return order if len(order) == numCourses else []
         
