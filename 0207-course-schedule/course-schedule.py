c5fee81f6fead5b70d469043716475db79d04f91
omarionnn <75476241+omarionnn@users.noreply.github.com>
2025-03-30 12:43:25 -0400

    Time: 7 ms (48.60%) Space: 18.8 MB (98.49%)

---
+++ 0207-course-schedule/course-schedule.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+        in_degree = [0] * numCourses
+        
+        for course, preq in prerequisites:
+            graph[preq].append(course)
+            in_degree[course] += 1
+            
+        queue = deque([i for i in range(numCourses) if in_degree[i] == 0])
+        courses_taken = 0
+
+        while queue:
+            course = queue.popleft()
+            courses_taken += 1
+
+            for c in graph[course]:
+                in_degree[c] -= 1
+                if in_degree[c] == 0:
+                    queue.append(c)
+
+
+        return courses_taken == numCourses
+        
