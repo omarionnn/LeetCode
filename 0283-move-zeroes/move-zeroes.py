5ff66b090204701e062eb343efda0fed4dce9e5c
omarionnn <75476241+omarionnn@users.noreply.github.com>
2025-06-16 12:08:45 -0400

    Time: 7 ms (44.29%) Space: 18.8 MB (78.70%)

--- 0283-move-zeroes/move-zeroes.py
+++ 0283-move-zeroes/move-zeroes.py
@@ -3,14 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        l = 0
 
-        left = 0
-
-        for right in range(len(nums)):
-
-            if nums[right] != 0:
-                nums[left], nums[right] = nums[right], nums[left]
-
-                left += 1
-        return nums
+        for r in range(len(nums)):
+            if nums[r] != 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+        
         
