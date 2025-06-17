9a50720c88a46a58809fb96a84a9db9985a4cb6c
omarionnn <75476241+omarionnn@users.noreply.github.com>
2025-06-17 12:32:17 -0400

    Time: 111 ms (84.31%) Space: 18.1 MB (48.75%)

--- 0424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -2,8 +2,8 @@ class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         state = {}
         start = 0
+        max_length = 0
         max_freq = 0
-        longest = 0
 
         for end in range(len(s)):
             state[s[end]] = state.get(s[end], 0) + 1
@@ -13,9 +13,8 @@ class Solution:
             while (end - start + 1) - max_freq > k:
                 state[s[start]] -= 1
                 start += 1
+            max_length = max(max_length, end - start + 1)
 
-            longest = max(longest, end - start + 1)
-
-        return longest
+        return max_length 
 
         
