c5a358afb84322fcc58b61fdc00e7867ba5088ab
omarionnn <75476241+omarionnn@users.noreply.github.com>
2025-06-23 23:18:21 -0400

    Time: 0 ms (100.00%) Space: 17.5 MB (97.11%)

---
+++ 0394-decode-string/decode-string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+
+        for char in s:
+            if char != ']':
+                stack.append(char)
+            else:
+                subs = ''
+                while stack and stack[-1] != '[':
+                    subs = stack.pop() + subs
+                stack.pop()
+
+                k = ''
+                while stack and stack[-1].isdigit():
+                    k = stack.pop() + k
+                stack.append(int(k) * subs)
+
+        return ''.join(stack)
+        
