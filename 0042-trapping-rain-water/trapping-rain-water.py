97a6763114eb8b3f6f497afda8fe8b4e1923a09e
Omari Nyarko <75476241+omarionnn@users.noreply.github.com>
2025-12-02 14:32:51 -0500

    Time: 11 ms (71.23%) Space: 19.2 MB (69.18%)

--- 0042-trapping-rain-water/trapping-rain-water.py
+++ 0042-trapping-rain-water/trapping-rain-water.py
@@ -4,14 +4,17 @@ class Solution:
         l, r = 0, len(height) - 1
         left, right = height[l], height[r]
 
+
         while l < r:
             if left < right:
                 l += 1
                 left = max(left, height[l])
-                res += abs(left - height[l])
+                res += (left - height[l])
+
             else:
                 r -= 1
                 right = max(right, height[r])
-                res += abs(right - height[r])
+                res += (right - height[r])
+
         return res
         
